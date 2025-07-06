f83f63deae8b6c14e712963b4ad31891b2b0198a
neojarvis <techadmin@iamneo.ai>
2025-07-06 13:28:47 +0000

    added view tasks feature

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Add Task");
         System.out.print("Mark Task as completed");
+        System.out.print("View Taskss");
     }
 
 }
